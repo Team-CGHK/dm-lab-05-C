9896208578ffce5b7fcd12015a6118e45222281d
h0tk3y <hotkeytlt@gmail.com>
2014-02-08 15:40:36 +0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+
+namespace DiscreteMathLab5_C
+{
+    class Program
+    {
+        static StreamReader sr = new StreamReader("num2choose.in");
+        static StreamWriter sw = new StreamWriter("num2choose.out");
+
+        static void Main(string[] args)
+        {
+            string[] parts = sr.ReadLine().Split(' ');
+            int n = int.Parse(parts[0]),
+                k = int.Parse(parts[1]);
+            long m = int.Parse(parts[2]);
+            int[] result = GetChooseByNumber(n, k, m);
+            for (int i = 0; i < k; i++)
+            {
+                sw.Write(result[i] + " ");
+            }
+            sw.Close();
+        }
+
+        static int[] GetChooseByNumber(int n, int k, long number)
+        {
+            int[] set = new int[n];
+            for (int i = 0; i < n; i++)
+                set[i] = i + 1;
+            int[] choose = new int[k];
+            int pos = 0;
+            while (pos < k)
+            {
+                int delta = 0;
+                int nn = set.Length-1;
+                while (number - C(nn, k - pos - 1) >= 0)
+                {
+                    number -= C(nn--, k - pos - 1);
+                    delta++;
+                }
+                choose[pos] = set[delta];
+                int[] nextset = new int[set.Length - delta - 1];
+                int j = 0;
+                for (long i = delta + 1; i < set.Length; i++)
+                     nextset[i-delta-1] = set[i];
+                set = nextset;
+                pos++;
+            }
+            return choose;
+        }
+
+        static long C(int n, int k)
+        {
+            double divident = 1;
+            int j = 2;
+            for (int i = 0; i < k; i++)
+            {
+                divident *= n - i;
+                if (j <= k) divident /= j++;
+            }
+            for ( ; j<=k; j++)
+                divident /= j++;
+            return (long)Math.Round(divident);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("DiscreteMathLab5_C")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DiscreteMathLab5_C")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("523776b9-c9ec-4692-b510-8b59c90d4e42")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
